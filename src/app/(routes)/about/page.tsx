7b69034a0eed542ef875040394cee720af86ac03
Nikesh Rajbhar <nikesh21soni@gmail.com>
2025-04-14 06:49:58 +0530

    Add video component to About page and comment out Education section

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,13 +1,39 @@
+"use client"
+import { useEffect, useRef } from "react";
 import BlurFade from "@/components/magicui/blur-fade";
 import { ResumeCard } from "@/components/resume-card";
 import { DATA } from "@/data/resume";
 import Markdown from "react-markdown";
+// import viseosCode  form "../"
 
 const BLUR_FADE_DELAY = 0.04;
 
 export default function About() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.5; // Set to 0.5 for half-speed playback
+    }
+  }, []);
+
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
+
+      <div className="flex justify-start">
+        <div className="relative w-[50%] h-0 pb-[28.125%] sm:pb-[28.125%]">
+          <video
+            ref={videoRef}
+            className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
+            autoPlay
+            loop
+            muted
+          >
+            <source src="/new-video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
       <section id="about">
         <BlurFade delay={BLUR_FADE_DELAY * 3}>
           <h2 className="text-xl font-bold">About</h2>
@@ -18,7 +44,7 @@ export default function About() {
           </Markdown>
         </BlurFade>
       </section>
-      
+
       <section id="education">
         <div className="flex min-h-0 flex-col gap-y-3">
           <BlurFade delay={BLUR_FADE_DELAY * 7}>
